f1ab82acb1ca9ebb87f3986efa917e3407d09e8f
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-02-01 16:41:47 +0100

    Print out the average segmentation count for a transcription

--- src/extract_segmentations.py
+++ src/extract_segmentations.py
@@ -2,6 +2,7 @@ from os import getenv, path
 from json import load
 from collections import defaultdict
 from morph import parseMorph
+from statistics import mean
 directory = 'data'
 infile = path.join(directory, 'Dictionary.json')
 assert path.exists(infile)
@@ -16,6 +17,7 @@ dictionary = json_data['dictionary']
 data = defaultdict(list)
 def is_fragment(form: str) -> bool:
   return '[' in form or ']' in form or 'x' in form or '(-)' in form
+segmentation_counts = list[int]()
 for transcription, values in dictionary.items():
   if (not is_fragment(transcription)
       and transcription.strip() != ''
@@ -26,8 +28,10 @@ for transcription, values in dictionary.items():
       morph = parseMorph(value)
       if morph.segmentation != '':
         segmentations.append(morph.segmentation.lower())
+    segmentation_counts.append(len(segmentations))
     joined_segmentations = join(segmentations)
     data[joined_segmentations].append(transcription)
+print('Average segmentation count for a transcription: {0:2f}'.format(mean(segmentation_counts)))
 for l in data.values():
   l.sort()
 with open(outfile, 'w', encoding='utf-8') as fout:
