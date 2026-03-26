929188c4af21941f4ab3eacf1d367780f5f8dc43
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-03-26 01:12:43 +0100

    Use Pandas for dataset construction, detect, print and drop duplicates

--- src/extract_segmentations.py
+++ src/extract_segmentations.py
@@ -1,40 +1,58 @@
-from os import getenv, path
+from os import path
 from json import load
-from collections import defaultdict
 from morph import parseMorph
-from statistics import mean
-directory = 'data'
-infile = path.join(directory, 'Dictionary.json')
-assert path.exists(infile)
-outfile = 'word.xhu.tsv'
-SEP = ', '
-def join(words: list[str]) -> str:
-  return SEP.join(sorted(words))
-assert path.exists(infile)
-with open(infile, 'r', encoding='utf-8') as fin:
-  json_data = load(fin)
-dictionary = json_data['dictionary']
-data = defaultdict(list)
+import argparse
+import pandas as pd
+from morphosyntactic_word import MorphosyntacticWord
+
 def is_fragment(form: str) -> bool:
   return '[' in form or ']' in form or 'x' in form or '(-)' in form
-segmentation_counts = list[int]()
+
+parser = argparse.ArgumentParser(
+  prog='extract_segmentations.py',
+  description='Extract morpheme segmentations from a morphological dictionary'
+)
+parser.add_argument('infile', help='The dictionary file')
+parser.add_argument('outfile', help='The resulting dataset file')
+parser.add_argument('graphic_variants', help='A file for graphic variants')
+parser.add_argument('homonyms', help='A file for homonyms')
+args = parser.parse_args()
+
+if not path.exists(args.infile):
+  print('The dictionary file does not exist.')
+  exit()
+
+with open(args.infile, 'r', encoding='utf-8') as fin:
+  json_data = load(fin)
+dictionary = json_data['dictionary']
+
+morphosyntactic_words = set[MorphosyntacticWord]()
+
 for transcription, values in dictionary.items():
   if (not is_fragment(transcription)
       and transcription.strip() != ''
       and transcription.islower()
-      and not any(char.isdigit() for char in transcription)):
-    segmentations = list[str]()
+      and not any(char.isdigit() for char in transcription)
+      and not transcription.startswith('*')):
     for value in values:
       morph = parseMorph(value)
-      if morph.segmentation != '':
-        segmentations.append(morph.segmentation.lower())
-    segmentation_counts.append(len(segmentations))
-    joined_segmentations = join(segmentations)
-    data[joined_segmentations].append(transcription)
-print('Average segmentation count for a transcription: {0:2f}'.format(mean(segmentation_counts)))
-for l in data.values():
-  l.sort()
-with open(outfile, 'w', encoding='utf-8') as fout:
-  for joined_segmentations, transcriptions in sorted(data.items()):
-    joined_transcriptions = join(transcriptions)
-    print(joined_segmentations, joined_transcriptions, sep='\t', file=fout)
+      if morph.segmentation != '' and not morph.segmentation.startswith('-') and morph.pos != 'unclear':
+        segmentation = morph.segmentation.lower()
+        morphosyntactic_word = MorphosyntacticWord(transcription, segmentation, morph.pos)
+        morphosyntactic_words.add(morphosyntactic_word)
+
+df = pd.DataFrame(sorted(morphosyntactic_words))
+
+df.sort_values(['segmentation', 'pos'], inplace=True)
+df[df.duplicated(['segmentation', 'pos'], False)].to_csv(args.graphic_variants, sep='\t', index=False)
+
+df.sort_values(['form', 'pos'], inplace=True)
+df[df.duplicated(['form', 'pos'], False)].to_csv(args.homonyms, sep='\t', index=False)
+
+df.sort_values(['segmentation', 'pos'], inplace=True)
+df.drop_duplicates(['segmentation', 'pos'], inplace=True)
+
+df.sort_values(['form', 'pos'], inplace=True)
+df.drop_duplicates(['form', 'pos'], inplace=True)
+
+df.to_csv(args.outfile, sep='\t', index=False)
---
+++ src/morphosyntactic_word.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True, order=True)
+class MorphosyntacticWord:
+  form: str
+  segmentation: str
+  pos: str
